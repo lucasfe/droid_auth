67f56136eefe115e7762ab5134652e5b2ace9ae0
Lucas de Oliveira Ferreira <lucasfe@inatel.br>
2018-03-23 13:48:14 -0300

    Adding mvvm on user
    Signed-off-by: Lucas de Oliveira Ferreira <lucasfe@inatel.br>

--- app/src/main/java/br/com/lucasfe/droidauth/model/UserViewModel.kt
+++ app/src/main/java/br/com/lucasfe/droidauth/model/UserViewModel.kt
@@ -1,6 +1,8 @@
 package br.com.lucasfe.droidauth.model
 
 import android.databinding.BaseObservable
+import android.databinding.Bindable
+import br.com.lucasfe.droidauth.BR
 import java.util.*
 
 class UserViewModel(private val user: User) : Observer, BaseObservable() {
@@ -10,7 +12,19 @@ class UserViewModel(private val user: User) : Observer, BaseObservable() {
     }
 
     override fun update(p0: Observable?, p1: Any?) {
+
+        if (p1 is String) {
+            if (p1 == "name") {
+                notifyPropertyChanged(BR.name)
+            }
+        }
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    val name: String
+    @Bindable get() {
+        return user.name
     }
 
 }
